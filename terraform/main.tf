4e6cb0baf85e285401cdf23c0e585029ff8cbb3a
Imesh Senanayake <imeshs@n-able.biz>
2026-06-16 13:52:10 +0530

    feat: add ArgoCD Helm release and k8s namespace resources to terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,15 @@ resource "helm_release" "argocd" {
   create_namespace = true
   version          = "5.51.6"
 }
+
+resource "kubernetes_namespace" "k8s" {
+  metadata {
+    annotations = {
+      name = "k8s"
+    }
+    labels = {
+      mylabel = "k8s"
+    }
+    name = "k8s"
+  }
+}
