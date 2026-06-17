e266f94d64d395ecb8692fbd44a4a6a16506d850
Imesh Senanayake <imeshs@n-able.biz>
2026-06-17 21:28:07 +0530

    feat: add Grafana Helm release resource to monitoring namespace

--- terraform/grafana.tf
+++ terraform/grafana.tf
@@ -2,6 +2,7 @@ resource "helm_release" "grafana" {
   name             = "grafana"
   repository       = "https://grafana-community.github.io/helm-charts"
   chart            = "grafana"
+  version          = "9.5.9"
   namespace        = "monitoring"
   create_namespace = true
 }
