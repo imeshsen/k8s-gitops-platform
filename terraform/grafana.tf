9119999424c35aac19841d74722733798bc03019
Your Name <you@example.com>
2026-06-18 05:03:15 +0000

    added

--- terraform/grafana.tf
+++ terraform/grafana.tf
@@ -2,7 +2,7 @@ resource "helm_release" "grafana" {
   name             = "grafana"
   repository       = "https://grafana-community.github.io/helm-charts"
   chart            = "grafana"
-  version          = "9.5.9"
+  version          = "10.5.15"
   namespace        = "monitoring"
   create_namespace = true
 }
