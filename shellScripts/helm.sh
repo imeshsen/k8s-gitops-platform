d6524c86784a92576feab9d3ac5936a396d382ac
Your Name <you@example.com>
2026-06-18 07:54:54 +0000

    removed

--- shellScripts/helm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for chart in backend frontend
-do
-  echo "deploying $chart..........."
-  helm upgrade --install $chart ../helm/$chart \
-    --namespace k8s \
-    --create-namespace
-done
