cd6187ce1e32d3af50348cafdd9d6e0307ecc8c6
Imesh Senanayake <imeshs@n-able.biz>
2026-06-26 23:33:58 +0530

    feat: add Prometheus helm release to terraform configuration

--- terraform/prometheus.tf
+++ terraform/prometheus.tf
@@ -1,13 +1,13 @@
 resource "helm_release" "prom" {
-  name             = "prometheus"
-  repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "prometheus"
-  version          = "29.12.0"
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+  version    = "29.12.0"
 
   namespace        = "monitoring"
   create_namespace = true
 
   values = [
-    file("${../prometheus/prometheus.yml")
+    file("${path.module}/../prometheus/prometheus.yml")
   ]
-}
+}
