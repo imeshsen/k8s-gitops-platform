d9157dbe56bf8e432874774ee5666298b7449cf9
Imesh Senanayake <imeshs@n-able.biz>
2026-06-26 23:37:35 +0530

    feat: add docker-compose and prometheus configuration for monitoring stack

--- terraform/grafana.tf
+++
@@ -1,8 +0,0 @@
-resource "helm_release" "grafana" {
-  name             = "grafana"
-  repository       = "https://grafana-community.github.io/helm-charts"
-  chart            = "grafana"
-  version          = "10.5.15"
-  namespace        = "monitoring"
-  create_namespace = true
-}
