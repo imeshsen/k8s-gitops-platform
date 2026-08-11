ab51c7887aea7e900850277f10224af032afabb3
Imesh Senanayake <imeshs@n-able.biz>
2026-08-12 01:23:09 +0530

    feat: remove default values for username, password, and namespace variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,15 @@
 variable "username" {
   type        = string
   description = "The username for the generic secret"
-  default     = "imesh"
 }
 
 variable "password" {
   type        = string
   description = "The password for the generic secret"
-  default     = "1234"
   sensitive   = true
 }
 
 variable "namespace" {
   type        = string
   description = "The namespace for the applications"
-  default     = "k8s"
 }
