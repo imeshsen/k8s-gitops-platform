56367557ab715bf2cf95299de557cfb652e17e92
Imesh Senanayake <imeshs@n-able.biz>
2026-08-12 15:00:50 +0530

    feat: remove unused variables for Helm source, version, and kube config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,22 +14,3 @@ variable "namespace" {
   description = "The namespace for the applications"
 }
 
-variable "terraform_source" {
-    type = string
-    description = "Helm source"
-}
-
-variable "terraform_version" {
-    type = string
-    description = "Helm version"
-}
-
-variable "config_path" {
-  type        = string
-  description = "The path for the kube config"
-}
-
-variable "config_context" {
-  type        = string
-  description = "The cluster name"
-}
