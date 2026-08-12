c0efb4f976df017a2d9d56a90058afa7ccd99f0a
Imesh Senanayake <imeshs@n-able.biz>
2026-08-12 16:15:48 +0530

    feat: remove deprecated Terraform resources and configurations

--- terraform/main.tf
+++
@@ -1,8 +0,0 @@
-resource "helm_release" "argocd" {
-  name             = "argocd"
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argo-cd"
-  namespace        = "argocd"
-  create_namespace = true
-  version          = "5.51.6"
-}
